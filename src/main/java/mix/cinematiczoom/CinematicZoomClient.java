dedb8b15203afa64dde7aa8da9eb960e521e4c3e
mel1x <75519364+mel1x@users.noreply.github.com>
2026-02-15 18:08:41 +0500

    1.21.11 Update

--- src/main/java/mix/cinematiczoom/CinematicZoomClient.java
+++ src/main/java/mix/cinematiczoom/CinematicZoomClient.java
@@ -5,12 +5,16 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class CinematicZoomClient implements ClientModInitializer {
 
     public static final String MODID = "cinematiczoom";
     public static KeyBinding ZOOM_KEYBIND;
+
+    private static final KeyBinding.Category ZOOM_CATEGORY =
+            KeyBinding.Category.create(Identifier.of(MODID, "main"));
 
     @Override
     public void onInitializeClient() {
@@ -20,7 +24,7 @@ public class CinematicZoomClient implements ClientModInitializer {
                 "key.cinematiczoom.zoom",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_C,
-                "key.categories.cinematiczoom"
+                ZOOM_CATEGORY
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> ZoomManager.tick(client, ZOOM_KEYBIND));
