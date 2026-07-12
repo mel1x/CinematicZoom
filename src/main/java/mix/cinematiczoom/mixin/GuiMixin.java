be24ccb6b6026727d059f08d6a95f323190669ee
mel1x <75519364+mel1x@users.noreply.github.com>
2026-07-12 22:42:51 +0500

    Migrate GameRendererMixin to GuiMixin for 26.2

--- src/main/java/mix/cinematiczoom/mixin/GuiMixin.java
+++ src/main/java/mix/cinematiczoom/mixin/GuiMixin.java
@@ -2,15 +2,15 @@ package mix.cinematiczoom.mixin;
 
 import mix.cinematiczoom.ZoomManager;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.gui.Gui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(GameRenderer.class)
-public class GameRendererMixin {
+@Mixin(Gui.class)
+public class GuiMixin {
 
-    @ModifyVariable(method = "extractGui(Lnet/minecraft/client/DeltaTracker;ZZ)V", at = @At("STORE"), ordinal = 0)
+    @ModifyVariable(method = "extractRenderState(Lnet/minecraft/client/DeltaTracker;ZZ)V", at = @At("STORE"), ordinal = 0)
     private GuiGraphicsExtractor cinematiczoom$drawBarsFirst(GuiGraphicsExtractor ctx) {
         // Мы рендерим полосы сразу после создания GuiGraphicsExtractor
         // Это гарантирует, что они будут позади всех остальных элементов GUI (включая hotbar),
